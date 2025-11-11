bd100c69829959869863a73a10f244d63dc3740f
ccanalia82 <132602442+ccanalia82@users.noreply.github.com>
2025-11-11 13:27:46 -0700

    Create testClient.js

---
+++ src/testClient.js
@@ -0,0 +1,16 @@
+const AladdinClient = require("./src/aladdinClient");
+
+const client = new AladdinClient({
+  username: process.env.GENIE_USER,
+  password: process.env.GENIE_PASS,
+  debug: true,
+});
+
+(async () => {
+  try {
+    const status = await client.getStatus();
+    console.log("Garage Door Status:", status);
+  } catch (err) {
+    console.error("Error:", err);
+  }
+})();
